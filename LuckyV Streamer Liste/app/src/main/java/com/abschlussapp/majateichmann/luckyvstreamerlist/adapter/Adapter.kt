154a422cb7437c295892062567557b1ff3e1a26c
MajaTeichmann <katharina.nickname@hotmail.de>
2023-04-28 15:38:22 +0200

    28.04.2023 -> 15:38 Uhr

--- LuckyV Streamer Liste/app/src/main/java/com/abschlussapp/majateichmann/luckyvstreamerlist/adapter/Adapter.kt
+++ LuckyV Streamer Liste/app/src/main/java/com/abschlussapp/majateichmann/luckyvstreamerlist/adapter/Adapter.kt
@@ -1,5 +1,29 @@
 package com.abschlussapp.majateichmann.luckyvstreamerlist.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.abschlussapp.majateichmann.luckyvstreamerlist.data.datamodels.Streamer
 
 
+class Adapter(
+private val context: Context,
+private val dataset: List<Streamer>
+) : RecyclerView.Adapter<Adapter.ItemViewHolder>() {
 
+    class ItemViewHolder(val binding: StreamerItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val binding = StreamerItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ItemViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        var streamer = dataset[position]
+    }
+}
