1888862083ef340f4ea2e1e0fd934aae4816fb24
MajaTeichmann <katharina.nickname@hotmail.de>
2023-04-28 11:47:55 +0200

    28.04.2023 -> 11:47 Uhr

--- LuckyV Streamer Liste/app/src/main/java/com/abschlussapp/majateichmann/luckyvstreamerlist/ui/StartFragment.kt
+++ LuckyV Streamer Liste/app/src/main/java/com/abschlussapp/majateichmann/luckyvstreamerlist/ui/StartFragment.kt
@@ -5,12 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import com.abschlussapp.majateichmann.luckyvstreamerlist.R
+import com.abschlussapp.majateichmann.luckyvstreamerlist.data.remote.StreamerApi
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,16 +16,9 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class StartFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -36,25 +27,28 @@ class StartFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_start, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StartFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StartFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+//        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+//
+//// Aktivieren Sie die ProgressBar
+//        progressBar.visibility = View.VISIBLE
+//
+//// Führen Sie den API-Aufruf aus
+//        StreamerApi.execute(object : Callback {
+//            override fun onSuccess(result: ApiResponse) {
+//                // Aktualisieren Sie die ProgressBar
+//                progressBar.progress = 100
+//
+//                // Verstecken Sie die ProgressBar
+//                progressBar.visibility = View.GONE
+//            }
+//
+//            override fun onError(error: String) {
+//                // Verstecken Sie die ProgressBar
+//                progressBar.visibility = View.GONE
+//            }
+//        }
+//        )
+
     }
 }
