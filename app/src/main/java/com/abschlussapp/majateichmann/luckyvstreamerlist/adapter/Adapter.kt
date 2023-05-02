42c1085344ccef107a4785c912a23e0c38ff0853
MajaTeichmann <katharina.nickname@hotmail.de>
2023-05-02 11:32:07 +0200

    02.05.2023 -> 11:31 Uhr

--- app/src/main/java/com/abschlussapp/majateichmann/luckyvstreamerlist/adapter/Adapter.kt
+++ app/src/main/java/com/abschlussapp/majateichmann/luckyvstreamerlist/adapter/Adapter.kt
@@ -1,19 +1,25 @@
 package com.abschlussapp.majateichmann.luckyvstreamerlist.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.abschlussapp.majateichmann.luckyvstreamerlist.data.datamodels.Streamer
 
-
+/**
+ * Diese Klasse organisiert mithilfe der ViewHolder Klasse das Recycling
+ */
 class Adapter(
-private val context: Context,
 private val dataset: List<Streamer>
-) : RecyclerView.Adapter<Adapter.ItemViewHolder>(), ListAdapter {
+) : RecyclerView.Adapter<Adapter.ItemViewHolder>(){
 
-    class ItemViewHolder(val binding: StreamerItemBinding) : RecyclerView.ViewHolder(binding.root)
+    /**
+     * der ViewHolder umfasst die View und stellt einen Listeneintrag dar
+     */
+    inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val binding = StreamerItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
