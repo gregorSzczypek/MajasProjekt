92e8895e4965662f534fd2898c4d8f501a4bc5e7
MajaTeichmann <katharina.nickname@hotmail.de>
2023-05-03 11:48:26 +0200

    03.05.2023 -> 11:48 Uhr

--- app/src/main/java/com/abschlussapp/majateichmann/luckyvstreamerlist/MainActivity.kt
+++ app/src/main/java/com/abschlussapp/majateichmann/luckyvstreamerlist/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.abschlussapp.majateichmann.luckyvstreamerlist
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.abschlussapp.majateichmann.luckyvstreamerlist.databinding.ActivityMainBinding
@@ -13,5 +16,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+
+        //todo: wenn api noch nicht komplett geladen, schalte appname+logo visibility auf gone(nicht sichtbar)
+        //todo: wenn api fertig geladen (also screen 2 zu sehen) schalte appname+logo visibility auf visible (sichtbar)
+
+        return super.onCreateView(name, context, attrs)
     }
 }
